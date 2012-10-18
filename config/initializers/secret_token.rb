64a79dbb3313aee1d56d2e367a8c310fd8ce26eb
kumabotz <kumabotz@gmail.com>
2012-10-19 00:31:35 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Facetime::Application.config.secret_token = '5848473fad5143a268243d8951d2882b9e28dcc8bc4af32aec63564ce58f2e86c7e23cc3b7dfe5dc3e1460f5dc47e862877497858f8e40625f40eb658047db37'
