ad5ad027227ed9746a7930d09e09f61bb0c54e07
kumabotz <kumabotz@gmail.com>
2012-10-19 02:01:12 +0800

    Add queue and index route

--- config/routes.rb
+++ config/routes.rb
@@ -5,13 +5,14 @@ Facetime::Application.routes.draw do
   # Sample of regular route:
   #   match 'products/:id' => 'catalog#view'
   # Keep in mind you can assign values other than :controller and :action
-
+  match 'queue' => 'sessions#queue'
   # Sample of named route:
   #   match 'products/:id/purchase' => 'catalog#purchase', :as => :purchase
   # This route can be invoked with purchase_url(:id => product.id)
 
   # Sample resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
+  resources :sessions
 
   # Sample resource route with options:
   #   resources :products do
@@ -48,7 +49,7 @@ Facetime::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'sessions#index'
+  root :to => 'sessions#index'
 
   # See how all your routes lay out with "rake routes"
 
