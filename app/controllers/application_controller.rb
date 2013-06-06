d82b8acd2d95df061207480232e42591dada6b9a
kumabotz <kumabotz@gmail.com>
2013-06-06 22:24:28 +0800

    Disable done page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  before_filter :done
+  # disable done page
+  # before_filter :done
 
   def done
     render :controller => 'sessions', :action => 'done'
